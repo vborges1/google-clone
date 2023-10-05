033ee52fed961a1ddbc1a5600e26a8c0f1940d52
Vitor Borges <vsilveira@techsocial.com.br>
2023-10-05 13:51:49 -0300

    feat: consulta de dados do google search api

--- src/app/search/web/page.tsx
+++ src/app/search/web/page.tsx
@@ -1,5 +1,16 @@
-export default function WebSearchPage() {
+import { IResultItems } from "@/shared/interfaces/resultItems.interface"
+
+export default async function WebSearchPage({ searchParams }: any) {
+  const { GOOGLE_API_KEY, GOOGLE_CONTEXT_API_KEY } = process.env
+  const response = await fetch(`https://www.googleapis.com/customsearch/v1?key=${GOOGLE_API_KEY}&cx=${GOOGLE_CONTEXT_API_KEY}&q=${searchParams.searchTerm}`)
+  const data = await response.json()
+  const results = data.items
+
   return (
-    <div>page</div>
+    <>
+      {results && results.map((result: IResultItems) => {
+        return <h1 key={result.cacheId}>{result.title}</h1>
+      })}
+    </>
   )
 }
---
+++ src/shared/interfaces/resultItems.interface.tsx
@@ -0,0 +1,13 @@
+export interface IResultItems {
+    kind: string,
+    title: string,
+    htmlTitle: string,
+    link: string,
+    displayLink: string,
+    snippet: string
+    htmlSnippet: string
+    cacheId: string
+    formattedUrl: string
+    htmlFormattedUrl: string
+    pagemap: any
+}
